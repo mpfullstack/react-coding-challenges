af81bb38dfe2f5024da9a33775d7f04471bcd4c4
mpfullstack <info@marcperez.cat>
2021-06-08 08:38:47 +0200

    Added theme context provider to home app

--- dark-mode/src/routes/index.js
+++ dark-mode/src/routes/index.js
@@ -1,5 +1,15 @@
+import React from 'react';
 import Home from './App';
+import { ThemeContext } from '../common/context/ThemeContext';
 
 // Use something like react-router-dom to manage multiple pages/routes
 
-export default Home;
+export default function HomeApp() {
+  const [theme, toggleTheme] = React.useState('light');
+
+  return (
+    <ThemeContext.Provider value={{toggle: toggleTheme, theme}}>
+      <Home />
+    </ThemeContext.Provider>
+  );
+};
