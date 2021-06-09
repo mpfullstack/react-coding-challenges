4c9d4303c9fed898bdfbcfc8c712ea5ed9668ddd
mpfullstack <info@marcperez.cat>
2021-06-09 12:13:15 +0200

    Added spotify config values

--- spootify/src/config.js
+++ spootify/src/config.js
@@ -2,7 +2,7 @@ export default {
   api: {
     baseUrl: 'https://api.spotify.com/v1',
     authUrl: 'https://accounts.spotify.com/api/token',
-    clientId: '',
-    clientSecret: ''
+    clientId: 'a83118e76d9b4b32b0fe3c34662c5799',
+    clientSecret: 'f25d1db5879a4366a7508a2bc2d37e04'
   }
 }
